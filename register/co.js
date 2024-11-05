87a1b7be7086c3f1fea883209abb63f64c7dc7ee
Elias Saadi <eliassaadi13@gmail.com>
2024-11-05 16:59:56 +0100

    Modification de la page de connexion

--- register/co.js
+++ register/co.js
@@ -9,3 +9,23 @@ signUpButton.addEventListener('click', () => {
 signInButton.addEventListener('click', () => {
 	container.classList.remove("right-panel-active");
 });
+
+
+
+//JS Mobile
+const signUpButton_mobile = document.getElementById('signUp_mobile');
+const signInButton_mobile = document.getElementById('signIn_mobile');
+const mediaQuery = window.matchMedia('(max-width: 1030px)');
+
+function handleMediaQueryChange(e) {
+    if (e.matches) {
+        signUpButton_mobile.removeAttribute("hidden");
+        signInButton_mobile.removeAttribute("hidden");
+    } else {
+        signUpButton_mobile.setAttribute("hidden", "");
+        signInButton_mobile.setAttribute("hidden", "");
+    }
+}
+
+handleMediaQueryChange(mediaQuery);
+mediaQuery.addEventListener('change', handleMediaQueryChange);
