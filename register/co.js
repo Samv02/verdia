e0760b9d57464ed87d6012751dc369fd4867bb21
EliasSaadi <eliassaadi13@gmail.com>
2024-11-06 00:25:52 +0100

    Ajout et petite modif

--- register/co.js
+++ register/co.js
@@ -9,3 +9,22 @@ signUpButton.addEventListener('click', () => {
 signInButton.addEventListener('click', () => {
 	container.classList.remove("right-panel-active");
 });
+
+
+//JS Mobile
+const signUpButton_mobile = document.getElementById('signUpMobile');
+const signInButton_mobile = document.getElementById('signInMobile');
+const mediaQuery = window.matchMedia('(max-width: 1030px)');
+
+function handleMediaQueryChange(e) {
+    if (e.matches) {
+        signUpButton_mobile.removeAttribute("hidden");
+        //signInButton_mobile.removeAttribute("hidden");
+    } else {
+        signUpButton_mobile.setAttribute("hidden", true);
+        //signInButton_mobile.setAttribute("hidden", true);
+    }
+}
+
+handleMediaQueryChange(mediaQuery);
+mediaQuery.addEventListener('change', handleMediaQueryChange);
