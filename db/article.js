30481edb13f73c918fc4ae510ec4e0206076fbba
theochevallier <theolemayne@gmail.com>
2024-11-06 15:45:23 +0100

    feat: ajout page articles

---
+++ db/article.js
@@ -0,0 +1,108 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
+import {
+    getAuth,
+    signInWithEmailAndPassword,
+    createUserWithEmailAndPassword,
+    signInWithPopup,
+    GoogleAuthProvider,
+    onAuthStateChanged,
+    signOut,
+} from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
+import {
+    getFirestore,
+    collection,
+    addDoc,
+    query,
+    where,
+    orderBy,
+    onSnapshot,
+    doc,
+    setDoc,
+    getDoc,
+    getDocs,
+    updateDoc,
+    Timestamp,
+} from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js";
+export const firebaseConfig = {
+    apiKey: "AIzaSyBSH-hvCVkKhhMrdtfnoqSZ0TZzjmPMulo",
+    authDomain: "verdia-4401d.firebaseapp.com",
+    projectId: "verdia-4401d",
+    storageBucket: "verdia-4401d.firebasestorage.app",
+    messagingSenderId: "639552213584",
+    appId: "1:639552213584:web:b1ede9db70fc1cceb63755",
+    measurementId: "G-YV1L49M89L",
+};
+
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const auth = getAuth();
+
+//GET
+export const getArticles = async () => {
+    const articlesRef = collection(db, "articles");
+    const querySnapshot = await getDocs(articlesRef);
+    const articles = [];
+    querySnapshot.forEach((doc) => {
+        articles.push(doc.data());
+    });
+    return articles;
+};
+
+export async function getAllThemes() {
+    const articlesCollection = collection(db, "articles"); // Remplacez "articles" par le nom de votre collection d'articles
+    const snapshot = await getDocs(articlesCollection);
+
+    // Utiliser un Set pour obtenir des thèmes uniques
+    const themes = new Set();
+
+    snapshot.forEach((doc) => {
+        const data = doc.data();
+        if (data.theme) {
+            themes.add(data.theme); // Ajoute le thème à l'ensemble (Set)
+        }
+    });
+
+    // Convertir l'ensemble en tableau
+    return Array.from(themes);
+}
+
+export async function getAllArticleByTheme(theme) {
+    const articlesCollection = collection(db, "articles"); // Remplacez "articles" par le nom de votre collection
+    const q = query(articlesCollection, where("theme", "==", theme)); // Filtre les articles avec le thème spécifié
+
+    try {
+        const snapshot = await getDocs(q);
+        if (snapshot.empty) {
+            console.log("Aucun article trouvé pour ce thème.");
+            return false;
+        } else {
+            const articles = [];
+            snapshot.forEach((doc) => {
+                articles.push({ id: doc.id, ...doc.data() });
+            });
+            console.log("Articles trouvés :", articles);
+            return articles;
+        }
+    } catch (error) {
+        console.error("Erreur lors de la récupération des articles :", error);
+    }
+}
+
+//INSERT
+export async function insertArticle(article) {
+    try {
+        // Référence vers la collection "articles" dans Firestore
+        const docRef = await addDoc(collection(db, "articles"), {
+            contenu: article.contenu,
+            theme: article.theme,
+            titre: article.titre,
+            createdAt: new Date(), // Ajoute un timestamp de création
+        });
+
+        console.log("Article ajouté avec l'ID :", docRef.id);
+        return docRef.id; // Retourne l'ID du document ajouté
+    } catch (e) {
+        console.error("Erreur lors de l'ajout de l'article :", e);
+        return false;
+    }
+}
---
+++ gestion-article.js
@@ -0,0 +1,198 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
+import {
+    getAuth,
+    onAuthStateChanged,
+    signOut,
+    updateEmail,
+    updatePassword,
+    EmailAuthProvider,
+    reauthenticateWithCredential,
+} from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
+import {
+    getFirestore,
+    collection,
+    addDoc,
+    query,
+    orderBy,
+    onSnapshot,
+} from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js";
+import { app } from "./script.js";
+
+import {
+    getAllThemes,
+    getAllArticleByTheme,
+    insertArticle,
+} from "./db/article.js";
+import { getUserDocument } from "./db/user.js";
+
+const auth = getAuth();
+
+document.addEventListener("DOMContentLoaded", initThemesDiv);
+
+async function initThemesDiv() {
+    // Appeler la fonction pour récupérer tous les thèmes
+    getAllThemes()
+        .then((themes) => {
+            console.log("Tous les thèmes :", themes);
+        })
+        .catch((error) => {
+            console.error("Erreur lors de la récupération des thèmes :", error);
+        });
+
+    const themesTableau = await getAllThemes();
+
+    //Construire les div pour chaque thème existant
+    themesTableau.forEach((theme) => {
+        const conteneur = document.getElementById("conteneur");
+        const div = document.createElement("div");
+        div.classList.add(
+            "flex",
+            "flex-col",
+            "border",
+            "border-black-200",
+            "rounded-lg",
+            "p-4",
+            "m-2",
+            "hover:bg-gray-100"
+        );
+        const p = document.createElement("p");
+        p.textContent = theme;
+        div.appendChild(p);
+        conteneur.appendChild(div);
+
+        //Ajout du listener sur la div
+        div.addEventListener("click", function () {
+            clickOnTheme(theme);
+        });
+    });
+
+    //Si l'utilisateur est admin, afficher bouton ajout article
+    onAuthStateChanged(auth, async (user) => {
+        //console.log(user.uid);
+        const infos = await getUserDocument(user.uid);
+        if (infos.isadmin) {
+            document.getElementById("writeArticle").style.display = "block";
+        } else {
+            document.getElementById("writeArticle").style.display = "none";
+        }
+    });
+}
+
+async function clickOnTheme(theme) {
+    const articles = await getAllArticleByTheme(theme);
+    const conteneur = document.getElementById("conteneur-article");
+    //Reinitialiser le conteneur d'article
+    eraseArticleConteneur();
+    //Construire les articles trouvés
+    const titreTheme = document.getElementById("titre-theme");
+    titreTheme.innerText = "Thème : " + theme;
+    //conteneur.appendChild(titreTheme);
+    articles.forEach((article) => {
+        const articleConteneur = document.createElement("div");
+        articleConteneur.classList.add(
+            "border",
+            "border-black-300",
+            "rounded",
+            "p-6",
+            "mb-4"
+        );
+        articleConteneur.id = article.id;
+        const titreArticle = document.createElement("h3");
+        titreArticle.classList.add("text-2xl", "font-bold");
+        titreArticle.innerText = article.titre;
+        articleConteneur.appendChild(titreArticle);
+        const paragrapheArticle = document.createElement("p");
+        paragrapheArticle.innerText = article.contenu;
+        articleConteneur.appendChild(paragrapheArticle);
+        conteneur.appendChild(articleConteneur);
+    });
+}
+
+function eraseArticleConteneur() {
+    const conteneur = document.getElementById("conteneur-article");
+    while (conteneur.firstChild) {
+        conteneur.removeChild(conteneur.firstChild);
+    }
+}
+
+window.onscroll = function () {
+    const button = document.getElementById("backToTopBtn");
+    if (window.scrollY > 200) {
+        // Si on a scrollé de plus de 200px
+        button.classList.remove("hidden");
+    } else {
+        button.classList.add("hidden");
+    }
+};
+
+document.getElementById("backToTopBtn").addEventListener("click", scrollToTop);
+
+function scrollToTop() {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+}
+
+document.getElementById("writeArticle").addEventListener("click", function () {
+    showInputPopup();
+});
+
+// Fonction pour afficher la popup
+async function showInputPopup() {
+    const allThemes = await getAllThemes();
+    const optionDataList = allThemes
+        .map((theme) => `<option value="${theme}"></option>`)
+        .join("");
+
+    const { value: formValues } = await Swal.fire({
+        title: "Ajouter un article",
+        width: "50%",
+        html:
+            `<input id="swal-input-theme" class="swal2-input w-3/4 p-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-400" placeholder="Thème" list="data">` +
+            `<datalist id="data">${optionDataList}</datalist>` +
+            `<input id="swal-input-title" class="swal2-input w-3/4" placeholder="Titre">` +
+            `<textarea id="swal-input-content" class="swal2-textarea w-3/4" placeholder="Contenu" rows="4"></textarea>`,
+        focusConfirm: false,
+        showCancelButton: true,
+        confirmButtonText: "Valider",
+        preConfirm: () => {
+            // Récupérer les valeurs des champs
+            const theme = document.getElementById("swal-input-theme").value;
+            const title = document.getElementById("swal-input-title").value;
+            const content = document.getElementById("swal-input-content").value;
+
+            // Validation : Vérifier que tous les champs sont remplis
+            if (!theme || !title || !content) {
+                Swal.showValidationMessage(
+                    `Tous les champs doivent être remplis`
+                );
+                return false;
+            }
+
+            // Retourner les valeurs pour utilisation après la fermeture de la popup
+            return { content, theme, title };
+        },
+    });
+
+    // Si l'utilisateur a validé, afficher les données
+    if (formValues) {
+        console.log("Données de l'article:", formValues);
+        const article = {
+            contenu: formValues.content,
+            theme: formValues.theme,
+            titre: formValues.title,
+        };
+        if (insertArticle(article)) {
+            Swal.fire({
+                icon: "success",
+                title: "Article ajouté",
+                text: `Thème : ${formValues.theme}, Titre : ${formValues.title}`,
+            });
+        } else {
+            Swal.fire({
+                icon: "error",
+                title: "Erreur dans l'ajout de l'article",
+            });
+        }
+    }
+}
+
+//Afficher bouton en fonction utilisateur admin ou pas
