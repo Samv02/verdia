eeb66c0a893e1ee7a47afa48b4cf135d198c3f2e
Elias Saadi <eliassaadi13@gmail.com>
2024-11-06 11:29:34 +0100

    responsive continue

--- register/co.js
+++ register/co.js
@@ -1,6 +1,7 @@
 const signUpButton = document.getElementById('signUp');
 const signInButton = document.getElementById('signIn');
 const container = document.getElementById('container');
+const signInContainer = document.getElementById('id="signInContainer"');
 
 signUpButton.addEventListener('click', () => {
 	container.classList.add("right-panel-active");
@@ -19,12 +20,20 @@ const mediaQuery = window.matchMedia('(max-width: 1030px)');
 function handleMediaQueryChange(e) {
     if (e.matches) {
         signUpButton_mobile.removeAttribute("hidden");
-        //signInButton_mobile.removeAttribute("hidden");
+        signInButton_mobile.removeAttribute("hidden");
     } else {
-        signUpButton_mobile.setAttribute("hidden", true);
-        //signInButton_mobile.setAttribute("hidden", true);
+        signUpButton_mobile.setAttribute("hidden", "");
+        signInButton_mobile.setAttribute("hidden", "");
     }
 }
 
 handleMediaQueryChange(mediaQuery);
 mediaQuery.addEventListener('change', handleMediaQueryChange);
+
+function slideToRegister(){
+    signInContainer.classList.add("slideToLeft");
+    
+    signUpButton_mobile.addEventListener('click', () => {
+        container.classList.remove("right-panel-active");
+    });
+}
